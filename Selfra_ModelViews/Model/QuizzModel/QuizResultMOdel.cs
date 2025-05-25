204188f02d32b5363355d7aeaf986b6e9bd8b146
ArchieAsano <ArchieAsano@users.noreply.github.com>
2025-05-25 19:30:12 +0700

    resultmodel

---
+++ Selfra_ModelViews/Model/QuizzModel/QuizResultMOdel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selfra_ModelViews.Model.QuizzModel
+{
+    public class QuizResultMOdel
+    {
+        public string? QuizName { get; set; }
+        public string? UserName { get; set; }
+        public int Score { get; set; }
+    }
+}
