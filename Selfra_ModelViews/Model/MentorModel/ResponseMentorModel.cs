884e86885f44a1b90ba60b97710e0151a01148f1
ArchieAsano <ArchieAsano@users.noreply.github.com>
2025-07-15 11:29:18 +0700

    change mentor view

--- Selfra_ModelViews/Model/MentorModel/ResponseMentorModel.cs
+++ Selfra_ModelViews/Model/MentorModel/ResponseMentorModel.cs
@@ -1,15 +1,18 @@
 ﻿
 
+using Selfra_ModelViews.Model.UserModel;
+
 namespace Selfra_ModelViews.Model.MentorModel
 {
     public class ResponseMentorModel
     {
-        public string? MentorId { get; set; }
+        public string? Id { get; set; }
         public Guid? UserId { get; set; }
         public string? Bio { get; set; }
         public string? ExpertiseAreas { get; set; }
         public float Rating { get; set; }
         public int TotalReviews { get; set; }
         public bool IsAvailable { get; set; }
+        public ResponseUserModel User   { get; set; }
     }
 }
