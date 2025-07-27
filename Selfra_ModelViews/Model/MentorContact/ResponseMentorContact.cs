a21d08de1ae10f157d70eac8722e07142e69ed94
Ha Tung <haduytung809@gmail.com>
2025-07-27 08:27:14 +0700

    fix model mentorcontact

--- Selfra_ModelViews/Model/MentorContact/ResponseMentorContact.cs
+++ Selfra_ModelViews/Model/MentorContact/ResponseMentorContact.cs
@@ -6,6 +6,7 @@ namespace Selfra_ModelViews.Model.MentorContact
     {
         public string? MentorContactId { get; set; }
         public string? MentorId { get; set; }
+        public string? MentorName { get; set; }
         public Guid? UserId { get; set; }
         public string? UserName { get; set; }
         public string? Message { get; set; }
